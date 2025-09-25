899f1ef31d4abc8da6c3c37cd367e6f047bbf6c2
senkxff <xxzarenf@gmail.com>
2025-09-25 17:53:46 +0300

    Create stars

---
+++ index/index.js
@@ -0,0 +1,9 @@
+// создание звезд
+function random(x, y) {
+    return (x + (Math.random() * (y - x)));
+}
+
+for (let i = 0; i < 100; i++)
+{
+    document.body.innerHTML += '<div class="star" style="animation-duration: ' + random(0, 50) + 's; top: ' + random(0, 100) + '%; left: ' + random(0,100) + '%;"></div>';
+}
